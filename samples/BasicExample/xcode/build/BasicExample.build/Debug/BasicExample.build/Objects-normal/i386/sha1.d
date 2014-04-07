66809cea6dea34e2bd723b7d47d2a2099aebf3df
charlie <coragano@gmail.com>
2014-04-07 17:17:44 -0400

    Removed build folder

--- samples/BasicExample/xcode/build/BasicExample.build/Debug/BasicExample.build/Objects-normal/i386/sha1.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/charlie/Documents/Development/_libs/cpp/Cinder_085/blocks/Cinder-Spacebrew/samples/BasicExample/blocks/Cinder-WebSocketPP/src/websocketpp/sha1/sha1.cpp \
-  /Users/charlie/Documents/Development/_libs/cpp/Cinder_085/blocks/Cinder-Spacebrew/samples/BasicExample/blocks/Cinder-WebSocketPP/src/websocketpp/sha1/sha1.h
